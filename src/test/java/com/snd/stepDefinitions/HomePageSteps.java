82ba243e85610f2efd858e212deabcd61b56c84c
jubersherkar <70776972+jubersherkar@users.noreply.github.com>
2024-12-29 15:33:14 +0530

    Update HomePageSteps.java

--- src/test/java/com/snd/stepDefinitions/HomePageSteps.java
+++ src/test/java/com/snd/stepDefinitions/HomePageSteps.java
@@ -141,7 +141,7 @@ public class HomePageSteps {
 	}
 	@Then("Available flights should be displayed")
 	public void available_flights_should_be_displayed() {
-		Assert.assertEquals(homePage.getPageTitle(), "Flights Result");
+		Assert.assertEquals(homePage.getPageTitle(), "Flights Resultqqq");
 	    log.info("Verified Flight search");
 	}
 }
